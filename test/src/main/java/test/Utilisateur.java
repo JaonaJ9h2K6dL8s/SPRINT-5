788848d0c5b72e43c5261455449d41ca612d8101
BonaToky <tramakaboana6@gmail.com>
2025-11-19 12:11:46 +0100

    sprint5
    demande mr naina

---
+++ test/src/main/java/test/Utilisateur.java
@@ -0,0 +1,36 @@
+package test;
+
+import com.monframework.ModelView;
+import com.monframework.Url;
+
+public class Utilisateur {
+    @Url("merci")
+        public ModelView getMerciMonamie() {
+            ModelView mv = new ModelView("/merci.jsp");
+            mv.setData("message", "Merci mon amie ❤️");
+            return mv;
+        }
+
+        @Url("bonjour")
+        public ModelView direBonjour() {
+            ModelView mv = new ModelView("/bonjour.jsp");
+            mv.setData("texte", "Bonjour tout le monde ! 👋 Bienvenue sur notre framework.");
+            return mv;
+        }
+
+        @Url("test")
+        public ModelView methodeTest() {
+            ModelView mv = new ModelView("/test.jsp");
+            mv.setData("resultat", "Ceci est un test réussi ! ✅");
+            return mv;
+        }
+
+        @Url("info")
+        public ModelView getInfo() {
+            ModelView mv = new ModelView("/info.jsp");
+            mv.setData("framework", "MonFramework");
+            mv.setData("date", new java.util.Date());
+            mv.setData("utilisateur", "Testeur");
+            return mv;
+        }
+}
---
+++ test/src/main/webapp/merci.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Merci</title>
+</head>
+<body>
+    <h1>Merci mon amie ! ❤️</h1>
+    <p>Je suis content que tu sois là.</p>
+</body>
+</html>
